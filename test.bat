eb4881de0eebbb2d948a0d107fea9735be5b3e9b
ObscureTube <obscuretube@outlook.com>
2021-11-28 23:42:44 -0500

    made changes

---
+++ test.bat
@@ -0,0 +1,4 @@
+@ECHO OFF
+ECHO Congratulations! Your first batch file was executed successfully.
+"C:\Program Files\Git\bin\sh.exe" --login test.sh
+PAUSE
---
+++ test.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+git add .
+git commit -am "made changes"
+git push -u origin master
+echo Press Enter...
+read
